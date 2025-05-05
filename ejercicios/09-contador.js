b9d39a2d2d287926be00439d601813731a51d22b
Abrahan <abrahanramirez2410@gmail.com>
2025-05-05 11:07:20 -0400

    buenas practicas

--- ejercicios/09-contador.js
+++ ejercicios/09-contador.js
@@ -1,10 +1,12 @@
 //Realizar contador que cuando presiones un boton este aumente su numero
 let conteo = 0; //variable a aumentar
 
-//creo dos variables de tipo constante y le asigno que es igual al numero a aumentar en el html
-//y botonIncremento sera igual al boton de incrementar del index.html
+//creo variables de tipo constante y le asigno que es igual al numero a aumentar en el html
+//y los const de botones sean igual a los botones del html
 const valorContador = document.getElementById('contador');
 const botonIncremento = document.getElementById('botonIncrementar');
+const botonDescender = document.getElementById('botonDescender');
+const botonReiniciar = document.getElementById('botonReiniciar');
 
 function incrementar(){
  conteo++;
